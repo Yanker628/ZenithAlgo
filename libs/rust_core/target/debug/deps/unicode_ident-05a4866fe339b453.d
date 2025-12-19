04c750ac239554d5d35240838c1f0934532ea204
yankerjang <yanker0628@outlook.com>
2025-12-20 05:38:47 +0800

    pass

--- libs/rust_core/target/debug/deps/unicode_ident-05a4866fe339b453.d
+++
@@ -1,8 +0,0 @@
-/Users/yanker/Code/ZenithAlgo/libs/rust_core/target/debug/deps/unicode_ident-05a4866fe339b453.d: /Users/yanker/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs /Users/yanker/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs
-
-/Users/yanker/Code/ZenithAlgo/libs/rust_core/target/debug/deps/libunicode_ident-05a4866fe339b453.rlib: /Users/yanker/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs /Users/yanker/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs
-
-/Users/yanker/Code/ZenithAlgo/libs/rust_core/target/debug/deps/libunicode_ident-05a4866fe339b453.rmeta: /Users/yanker/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs /Users/yanker/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs
-
-/Users/yanker/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs:
-/Users/yanker/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs:
--- libs/rust_core/target/debug/libzenithalgo_rust.d
+++
@@ -1,1 +0,0 @@
-/Users/yanker/Code/ZenithAlgo/libs/rust_core/target/debug/libzenithalgo_rust.dylib: /Users/yanker/Code/ZenithAlgo/libs/rust_core/src/lib.rs
